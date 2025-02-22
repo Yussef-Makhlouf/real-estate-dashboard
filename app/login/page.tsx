0117415f786b9df6215ecdabe8ea47daa6b95357
Yussef Makhlouf <yussef.ali.it@gmail.com>
2025-02-22 13:56:12 +0200

    Update page.tsx
    Signed-off-by: Yussef Makhlouf <yussef.ali.it@gmail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -88,7 +88,7 @@ export default function Login() {
             <div className="text-center">
               <div className="relative w-24 h-24 mx-auto mb-4">
                 <Image
-                  src="/Group.jpg"
+                  src="/Group.svg"
                   alt="Logo"
                   fill
                   className="object-contain"
